686aee7eddb1b6d60e086af031f3304efdce6502
sanjaithiyagaraj <92373720+sanjaithiyagaraj@users.noreply.github.com>
2026-07-17 10:11:00 +0530

    Delete smart-expense-tracker directory

--- smart-expense-tracker/src/main/java/com/sanjai/smartexpensetracker/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.sanjai.smartexpensetracker.entity;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN
-}
--- smart-expense-tracker/src/main/java/com/sanjai/smartexpensetracker/service/DashboardService.java
+++
@@ -1,8 +0,0 @@
-package com.sanjai.smartexpensetracker.service;
-
-import com.sanjai.smartexpensetracker.dto.DashboardDto;
-
-public interface DashboardService {
-
-    DashboardDto getDashboard(String email);
-}
